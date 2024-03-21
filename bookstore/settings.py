e307615a52954a73389b4a0b13f8f3447d3acafd
camargo <gui.ferreira.camargo@gmail.com>
2024-03-21 12:17:43 -0300

    more changes

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -57,7 +57,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
