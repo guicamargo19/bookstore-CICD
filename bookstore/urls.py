cd41bff21c7a392113d3f0dc57e28b524eb0ae6b
camargo <gui.ferreira.camargo@gmail.com>
2024-03-21 12:09:45 -0300

    More changes for deploy

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -19,10 +19,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path, include
 from rest_framework.authtoken.views import obtain_auth_token
-import debug_toolbar
 
 urlpatterns = [
-    path("__debug__/", include(debug_toolbar.urls)),
     path("admin/", admin.site.urls),
     re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
     re_path("bookstore/(?P<version>(v1|v2))/", include("product.urls")),
